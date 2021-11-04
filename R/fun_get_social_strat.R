c82ab57a23e4a22a6803d7ac009a9ae07985c6ea
Pratik Gupte <pratikgupte16@gmail.com>
2021-11-04 12:34:18 +0100

    add import data.table

--- R/fun_get_social_strat.R
+++ R/fun_get_social_strat.R
@@ -4,13 +4,14 @@
 #'
 #' @return A dataframe with social strategy assigned.
 #' @export
+#' @import data.table
 #'
 get_social_strategy = function(df) {
   assertthat::assert_that(
     all(c("sH", "sN") %in% names(df)),
     msg = "get_social_strat: data does not have social weights"
   )
-  data.table::setDT(df)
+  df = data.table::setDT(df)
   df[, strat_social := data.table::fcase(
     (sH > 0 & sN > 0), "agent tracking",
     (sH > 0 & sN < 0), "handler tracking",
@@ -26,6 +27,7 @@ get_social_strategy = function(df) {
 #' @return A dataframe with outcomes summarised by generation and social
 #' strategy.
 #' @export
+#' @import data.table
 #'
 get_social_outcomes = function(df) {
   # get social strategy
@@ -63,4 +65,36 @@ get_social_outcomes = function(df) {
   # join prop infec stats
   df = merge(df, inf_stats)
   df
+}
+
+
+#' Get disease burden per social class.
+#'
+#' @param df A dataframe with social weights and ecological outcomes.
+#'
+#' @return A dataframe of disease burden, the sum of the time infected per
+#' strategy in each generation.
+#' @import data.table
+#' @export
+#' 
+get_disease_burden = function(df) {
+  # get social strategy
+  df_ = get_social_strategy(df)
+  
+  # sum time infected per gen and strategy
+  df_ = df_[, list(inf_load = sum(t_infec)), by = c("gen", "strat_social")]
+  
+  df_ = df_[, inf_load_prop := inf_load / sum(inf_load), by = c("gen")]
+}
+
+get_tinfec_stats = function(df) {
+  # get social strategy
+  get_social_strategy(df)
+  
+  df = df[t_infec > 1, ]
+  
+  df[, list(
+    mean_tinfec = mean(t_infec),
+    sd_tinfec = sd(t_infec)
+  ), by = c("gen", "strat_social")]
 }
