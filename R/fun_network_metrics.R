2ccdf75e4f9795248d872f112cd66eacfc6fd689
Pratik Gupte <pratikgupte16@gmail.com>
2021-12-02 10:21:54 +0100

    get strategy networks

--- R/fun_network_metrics.R
+++ R/fun_network_metrics.R
@@ -32,7 +32,7 @@ get_wrapped_distance = function(x1, y1, x2, y2, max_dist = 50) {
 #'
 #' @param g An igraph or tidygraph object.
 #'
-#' @return A numeric global efficiency value.
+#' @return A data.table of edges with landscapes distances between nodes.
 #' @export
 get_edge_distance = function(g) {
   edges = data.frame(activate(g, edges))
@@ -59,4 +59,55 @@ get_edge_distance = function(g) {
   )
   edges[, distance := unlist(Map(x1, y1, x2, y2, f = get_wrapped_distance))]
   edges[, !c("x1", "y1", "x2", "y2")]
+}
+
+#' Get connections between social strategies.
+#'
+#' @param g An igraph or tidygraph object, with node data containing social
+#' strategy information.
+#'
+#' @return A tidygraph network of connections between social strategies.
+#' @export
+get_strategy_networks = function(g) {
+  edges = data.frame(activate(g, edges))
+  data.table::setDT(edges)
+  
+  nodes = data.frame(activate(g, nodes))
+  assertthat::assert_that(
+    "social_strat" %in% colnames(nodes),
+    msg = "get_strat_networks: social strategy is missing from node data"
+  )
+  data.table::setDT(nodes)
+  nodes = nodes[, c("id", "social_strat")]
+
+  # link nodes and edges
+  edges = data.table::merge.data.table(
+    edges, nodes,
+    by.x = "from",
+    by.y = "id"
+  )
+  edges = data.table::merge.data.table(
+    edges, nodes,
+    by.x = "to",
+    by.y = "id"
+  )
+  # get unique strategy pairs
+  edges[, c("ss1", "ss2") := list(
+    pmin(social_strat.x, social_strat.y),
+    pmax(social_strat.x, social_strat.y)
+  )]
+  # the edge data
+  edges = edges[, list(
+    mean_weight = mean(weight),
+    sum_weight = sum(weight),
+    sd_weight = sd(weight)
+  ), by = c("ss1", "ss2")]
+  # get node data for frequency
+  nodes = nodes[,.N, by = "social_strat"]
+
+  tidygraph::tbl_graph(
+    nodes = nodes,
+    edges = edges,
+    directed = FALSE
+  )
 }
