93ae77a8e0250f3f7bea1fef07f798090b1ea13f
Pratik Gupte <pratikgupte16@gmail.com>
2021-12-14 12:55:13 +0100

    script to use cluster

---
+++ scripts/00_use_cluster.R
@@ -0,0 +1,39 @@
+
+# load libraries
+library(snevo)
+
+# param file name
+date = Sys.time() |> str_replace_all(" |:", "_")
+param_file = glue("data/parameters/parameters_{date}.csv")
+
+# make parameter combinations
+snevo::make_parameter_file(
+  scenario = c(0, 1, 2),
+  popsize = 500,
+  nItems = 1440,
+  landsize = 60,
+  nClusters = 60,
+  clusterSpread = 1,
+  tmax = 100,
+  genmax = 5000,
+  range_food = 2,
+  range_agents = 2,
+  range_move = 1,
+  handling_time = 5,
+  regen_time = 60,
+  pTransmit = "0.05",
+  initialInfections = 20,
+  costInfect = 0.25,
+  nThreads = 2,
+  replicates = 5,
+  which_file = param_file
+)
+
+# try sending in a job
+snevo::use_cluster(
+  ssh_con = "p284074@peregrine.hpc.rug.nl",
+  password = password, 
+  script = "scripts/do_sim_cluster.R", 
+  template_job = "bash/main_job_maker.sh", 
+  parameter_file = "data/parameters/parameters_test.csv"
+)
