f15bb17b967c837060d5bca735d7c362cdc45472
Pratik Gupte <pratikgupte16@gmail.com>
2021-12-14 15:29:27 +0100

    correct lcation cd

--- bash/main_job_maker.sh
+++ bash/main_job_maker.sh
@@ -6,12 +6,12 @@
 #SBATCH --job-name=snevo_simulation
 #SBATCH --cpus-per-task=8
 #SBATCH --array=1-n_array
-#SBATCH --output=/home/p284074/snevo/data/output/snevo_output_%a.log
+#SBATCH --output=/home/p284074/pathomove/data/output/snevo_output_%a.log
 #SBATCH --mem=500
 
 pwd
 ml load R/4.1.0-foss-2021a
-cd snevo
+cd patho-move-evol
 echo "now in" 
 pwd
 Rscript some rscript here ${SLURM_ARRAY_TASK_ID}
