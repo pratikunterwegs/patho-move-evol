11ff93634ab509c23c934298ab11a6f4966585f5
Pratik Gupte <pratikgupte16@gmail.com>
2021-12-14 13:03:01 +0100

    more time

--- bash/main_job_maker.sh
+++ bash/main_job_maker.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-#SBATCH --time=03:00:00
+#SBATCH --time=03:10:00
 #SBATCH --partition=regular
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --job-name=snevo_simulation
+#SBATCH --cpus-per-task=8
 #SBATCH --array=1-n_array
 #SBATCH --output=/home/p284074/snevo/data/output/snevo_output_%a.log
-#SBATCH --mem=5000
+#SBATCH --mem=500
 
 pwd
-ml load R/4.0.0-foss-2020a
+ml load R/4.1.0-foss-2021a
 cd snevo
 echo "now in" 
 pwd
