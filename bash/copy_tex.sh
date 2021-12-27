d98e455c6cc88608bc07d2e180559bb6c20ce503
Pratik Gupte <pratikgupte16@gmail.com>
2021-12-27 20:06:57 +0100

    copy tex script

---
+++ bash/copy_tex.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# copy pdfs and name correctly
+rm docs/ms_pathomove_*.pdf
+
+cp figures/fig_0*.png ms-pathomove/figures
+
+cd ms-pathomove
+pdflatex manuscript.tex
+bibtex manuscript.aux
+pdflatex manuscript.tex
+
+cd ..
+
+cp -p ms-pathomove/manuscript.pdf docs/ms_pathomove_`date -I`.pdf
