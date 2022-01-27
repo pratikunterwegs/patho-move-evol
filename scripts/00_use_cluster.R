d76ae58b88494ad31715c99fdb840a223cf7c718
Pratik Gupte <pratikgupte16@gmail.com>
2022-01-27 12:21:34 +0100

    real use cluster script

--- scripts/00_use_cluster.R
+++ scripts/00_use_cluster.R
@@ -1,6 +1,5 @@
-
 # load libraries
-library(snevo)
+library(pathomove)
 library(stringr)
 library(glue)
 
@@ -8,35 +7,38 @@ library(glue)
 date = Sys.time() |> str_replace_all(" |:", "_")
 param_file = glue("data/parameters/parameters_{date}.csv")
 
-# make parameter combinations
-snevo::make_parameter_file(
+password = readLines("data/password")
+
+# prepare parameters
+pathomove::make_parameter_file(
   scenario = 2,
   popsize = 500,
-  nItems = 1440,
+  nItems = 1800,
   landsize = 60,
   nClusters = 60,
   clusterSpread = 1,
-  tmax = 10,
-  genmax = 20,
-  range_food = 2,
-  range_agents = 2,
+  tmax = 500,
+  genmax = 5000,
+  g_patho_init = 3000,
+  range_food = 1,
+  range_agents = 1,
   range_move = 1,
   handling_time = 5,
-  regen_time = 60,
+  regen_time = c(20, 50, 100),
   pTransmit = "0.05",
   initialInfections = 20,
-  costInfect = 0.25,
-  nThreads = 2,
-  replicates = 3,
+  costInfect = c(0.01, 0.02, 0.05),
+  nThreads = 1,
+  replicates = 5,
   which_file = param_file
 )
 
 # try sending in a job
-snevo::use_cluster(
+pathomove::use_cluster(
   ssh_con = "p284074@peregrine.hpc.rug.nl",
   password = password, 
-  script = "scripts/do_sim_snevo.R", 
-  folder = "patho-move-evol",
+  script = "scripts/do_sim_cluster.R",
+  folder = "patho-move-evol", 
   template_job = "bash/main_job_maker.sh", 
   parameter_file = param_file
 )
