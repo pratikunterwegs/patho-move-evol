d62314764bd049bb3af7668278c715776dfcfdb6
Pratik Gupte <pratikgupte16@gmail.com>
2022-01-27 12:19:45 +0100

    job maker script

--- bash/main_job_maker.sh
+++ bash/main_job_maker.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-#SBATCH --time=03:10:00
+#SBATCH --time=04:10:00
 #SBATCH --partition=regular
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --job-name=snevo_simulation
-#SBATCH --cpus-per-task=8
 #SBATCH --array=1-n_array
 #SBATCH --output=/home/p284074/patho-move-evol/data/output/snevo_output_%a.log
-#SBATCH --mem=8000
+#SBATCH --mem=5000
 
 pwd
 ml load R/4.1.0-foss-2021a
+ml load Boost/1.76.0-GCC-10.3.0
 cd patho-move-evol
 echo "now in" 
 pwd
