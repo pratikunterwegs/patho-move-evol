5fa8d54ed9e833cf5d6aa9fc31aa448e279e0a9f
Pratik Gupte <pratikgupte16@gmail.com>
2022-02-05 16:45:34 +0100

    new landscape versions

--- scripts/00_use_cluster.R
+++ scripts/00_use_cluster.R
@@ -16,10 +16,10 @@ param_file_default = glue("data/parameters/parameters_{date}_default.csv")
 pathomove::make_parameter_file(
   scenario = 2,
   popsize = 500,
-  nItems = 1800,
-  landsize = 60,
-  nClusters = 60,
-  clusterSpread = 1,
+  nItems = 2000,
+  landsize = 50,
+  nClusters = 200,
+  clusterSpread = 0.5,
   tmax = 100,
   genmax = 5000,
   g_patho_init = 3300,
@@ -48,45 +48,45 @@ pathomove::use_cluster(
   parameter_file = param_file_default
 )
 
-#### global case ###
+# #### global case ###
 
-# prepare parameters for global case
-param_file_global = glue("data/parameters/parameters_{date}_global.csv")
+# # prepare parameters for global case
+# param_file_global = glue("data/parameters/parameters_{date}_global.csv")
 
-pathomove::make_parameter_file(
-  scenario = 2,
-  popsize = 500,
-  nItems = 1800,
-  landsize = 60,
-  nClusters = 60,
-  clusterSpread = 1,
-  tmax = 100,
-  genmax = 5000,
-  g_patho_init = 3300,
-  range_food = 1,
-  range_agents = 1,
-  range_move = 1,
-  handling_time = 5,
-  regen_time = c(20, 50, 100),
-  pTransmit = "0.05",
-  initialInfections = 20,
-  costInfect = c(0.1, 0.25, 0.5),
-  nThreads = 1,
-  replicates = 5,
-  local_dispersal = FALSE,
-  infect_percent = FALSE,
-  which_file = param_file_global
-)
+# pathomove::make_parameter_file(
+#   scenario = 2,
+#   popsize = 500,
+#   nItems = 2000,
+#   landsize = 50,
+#   nClusters = 200,
+#   clusterSpread = 0.5,
+#   tmax = 100,
+#   genmax = 5000,
+#   g_patho_init = 3300,
+#   range_food = 1,
+#   range_agents = 1,
+#   range_move = 1,
+#   handling_time = 5,
+#   regen_time = c(20, 50, 100),
+#   pTransmit = "0.05",
+#   initialInfections = 20,
+#   costInfect = c(0.1, 0.25, 0.5),
+#   nThreads = 1,
+#   replicates = 5,
+#   local_dispersal = FALSE,
+#   infect_percent = FALSE,
+#   which_file = param_file_global
+# )
 
-# try sending in a job
-pathomove::use_cluster(
-  ssh_con = "p284074@peregrine.hpc.rug.nl",
-  password = password, 
-  script = "scripts/do_sim_pathomove.R",
-  folder = "patho-move-evol", 
-  template_job = "bash/main_job_maker_global.sh", 
-  parameter_file = param_file_global
-)
+# # try sending in a job
+# pathomove::use_cluster(
+#   ssh_con = "p284074@peregrine.hpc.rug.nl",
+#   password = password, 
+#   script = "scripts/do_sim_pathomove.R",
+#   folder = "patho-move-evol", 
+#   template_job = "bash/main_job_maker_global.sh", 
+#   parameter_file = param_file_global
+# )
 
 #### percent case ###
 
@@ -96,10 +96,10 @@ param_file_percent = glue("data/parameters/parameters_{date}_percent.csv")
 pathomove::make_parameter_file(
   scenario = 2,
   popsize = 500,
-  nItems = 1800,
-  landsize = 60,
-  nClusters = 60,
-  clusterSpread = 1,
+  nItems = 2000,
+  landsize = 50,
+  nClusters = 200,
+  clusterSpread = 0.5,
   tmax = 100,
   genmax = 5000,
   g_patho_init = 3300,
