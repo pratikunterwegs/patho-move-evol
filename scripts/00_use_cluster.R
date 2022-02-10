5e476a056afba0621b6ccef90de7a73a0bc49ddf
Pratik Gupte <pratikgupte16@gmail.com>
2022-02-10 14:40:49 +0100

    add latest params and runs

--- scripts/00_use_cluster.R
+++ scripts/00_use_cluster.R
@@ -8,45 +8,45 @@ date = Sys.time() |> str_replace_all(" |:", "_")
 
 password = readLines("data/password")
 
-#### default case ###
+### default case ####
 
 # prepare parameters for default case
-# param_file_default = glue("data/parameters/parameters_{date}_default.csv")
+param_file_default = glue("data/parameters/parameters_{date}_default.csv")
 
-# pathomove::make_parameter_file(
-#   scenario = 2,
-#   popsize = 500,
-#   nItems = 2000,
-#   landsize = 50,
-#   nClusters = 200,
-#   clusterSpread = 0.5,
-#   tmax = 100,
-#   genmax = 5000,
-#   g_patho_init = 3300,
-#   range_food = 1,
-#   range_agents = 1,
-#   range_move = 1,
-#   handling_time = 5,
-#   regen_time = c(20, 50, 100),
-#   pTransmit = "0.05",
-#   initialInfections = 20,
-#   costInfect = c(0.1, 0.25, 0.5),
-#   nThreads = 1,
-#   replicates = 5,
-#   local_dispersal = TRUE,
-#   infect_percent = FALSE,
-#   which_file = param_file_default
-# )
+pathomove::make_parameter_file(
+  scenario = 2,
+  popsize = 500,
+  nItems = 1800,
+  landsize = 60,
+  nClusters = 60,
+  clusterSpread = 1,
+  tmax = 100,
+  genmax = 5000,
+  g_patho_init = 3000,
+  range_food = 1,
+  range_agents = 1,
+  range_move = 1,
+  handling_time = 5,
+  regen_time = c(20, 50, 100),
+  pTransmit = "0.05",
+  initialInfections = 20,
+  costInfect = c(0.1, 0.25, 0.5),
+  nThreads = 1,
+  replicates = 10,
+  dispersal = 2.0, # for local-ish dispersal
+  infect_percent = FALSE,
+  which_file = param_file_default
+)
 
-# # try sending in a job
-# pathomove::use_cluster(
-#   ssh_con = "p284074@peregrine.hpc.rug.nl",
-#   password = password, 
-#   script = "scripts/do_sim_pathomove.R",
-#   folder = "patho-move-evol", 
-#   template_job = "bash/main_job_maker_default.sh", 
-#   parameter_file = param_file_default
-# )
+# try sending in a job
+pathomove::use_cluster(
+  ssh_con = "p284074@peregrine.hpc.rug.nl",
+  password = password, 
+  script = "scripts/do_sim_pathomove.R",
+  folder = "patho-move-evol", 
+  template_job = "bash/main_job_maker_default.sh", 
+  parameter_file = param_file_default
+)
 
 # #### global case ###
 
@@ -73,7 +73,7 @@ password = readLines("data/password")
 #   costInfect = c(0.1, 0.25, 0.5),
 #   nThreads = 1,
 #   replicates = 5,
-#   local_dispersal = FALSE,
+#   dispersal = 10, # for global-ish dispersal
 #   infect_percent = FALSE,
 #   which_file = param_file_global
 # )
@@ -96,24 +96,24 @@ param_file_percent = glue("data/parameters/parameters_{date}_percent.csv")
 pathomove::make_parameter_file(
   scenario = 2,
   popsize = 500,
-  nItems = 2000,
-  landsize = 50,
-  nClusters = 200,
-  clusterSpread = 0.5,
+  nItems = 1800,
+  landsize = 60,
+  nClusters = 60,
+  clusterSpread = 1,
   tmax = 100,
-  genmax = 1000,
-  g_patho_init = 500,
+  genmax = 5000,
+  g_patho_init = 3000,
   range_food = 1,
   range_agents = 1,
   range_move = 1,
   handling_time = 5,
-  regen_time = c(10, 20, 50, 100),
+  regen_time = c(20, 50, 100),
   pTransmit = "0.05",
   initialInfections = 20,
   costInfect = c(0.025, 0.05, 0.075),
   nThreads = 1,
   replicates = 10,
-  local_dispersal = FALSE,
+  dispersal = 2.0, # also local-ish dispersal
   infect_percent = TRUE,
   which_file = param_file_percent
 )
