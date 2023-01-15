f6f2787b8b9dd9eccdd0252fe506880f7c0ab2e3
Pratik Gupte <pratikgupte16@gmail.com>
2023-01-15 16:27:12 +0000

    Update and add job template shell scripts

---
+++ bash/main_job_maker_threshold.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --time=03:30:00
+#SBATCH --partition=regular
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --job-name=pathomove_sporadic
+#SBATCH --array=1-n_array
+#SBATCH --output=/home/p284074/patho-move-evol/data/output/pathomove_threshold_%a.log
+#SBATCH --mem=8000
+
+pwd
+ml load R/4.1.0-foss-2021a
+ml load Boost/1.76.0-GCC-10.3.0
+cd patho-move-evol
+echo "now in" 
+pwd
+Rscript some rscript here ${SLURM_ARRAY_TASK_ID}
