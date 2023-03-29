5687ed8f2ccf4716b5a8b7ae89c8d2c926586def
Pratik Gupte <pratikgupte16@gmail.com>
2023-03-29 19:20:17 +0100

    Update local runs script

--- scripts/00_make_batch_script.R
+++ scripts/00_make_batch_script.R
@@ -6,32 +6,40 @@ library(ggplot2)
 
 # param file name
 date <- Sys.time() |> str_replace_all(" |:", "_")
-param_file <- glue("data/parameters/parameters_{date}.csv")
 
-# make parameter combinations
+scenario_tag <- "default"
+# prepare parameters for default case
+param_file_default <- glue(
+  "data/parameters/parameters_{date}_{scenario_tag}.csv"
+)
+
 pathomove::make_parameter_file(
-  scenario = 2,
+  scenario = 1,
   popsize = 500,
   nItems = 1800,
   landsize = 60,
   nClusters = 60,
   clusterSpread = 1,
   tmax = 100,
-  genmax = 1000,
-  g_patho_init = 700,
+  genmax = 5000,
+  g_patho_init = 3000,
   range_food = 1,
   range_agents = 1,
   range_move = 1,
   handling_time = 5,
   regen_time = c(20, 50, 100),
-  pTransmit = "0.05",
+  pTransmit = 0.05,
   initialInfections = 20,
-  costInfect = c(0.01, 0.02, 0.05), # c(0.1, 0.25, 0.5),
-  nThreads = 8,
-  replicates = 1,
-  local_dispersal = FALSE,
-  infect_percent = TRUE,
-  which_file = param_file
+  costInfect = c(0.1, 0.25, 0.5),
+  multithreaded = FALSE,
+  replicates = 10,
+  dispersal = 2.0, # for local-ish dispersal
+  vertical = FALSE,
+  infect_percent = FALSE,
+  mProb = 0.01,
+  mSize = 0.01,
+  spillover_rate = 0.0,
+  which_file = param_file_default
 )
 
 # the R binary path
@@ -44,8 +52,10 @@ rscript <- "scripts/do_sim_pathomove.R"
 row_number <- seq(nrow(fread(param_file)))
 
 # make commands
-lines <- c("cd ../", glue("{Rbin} {rscript} {param_file} {row_number}"))
-
+lines <- c(
+  "cd ../",
+  glue("{Rbin} {rscript} {param_file} {row_number} {scenario_tag}")
+)
 
 # write batch file
 writeLines(
